146af2b9123e38f6c269d32856361079b31f87c9
Gustavo-Fernandes-umc <11221104197@alunos.umc.br>
2025-10-31 20:17:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// serve todos os arquivos estáticos da pasta raiz do projeto
+ 
 app.use(express.static(path.join(__dirname)));
 
-// fallback pro index.html (mantém navegação por âncora/SPA)
-app.get("*", (_req, res) => {
+app.get(/.*/, (_req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
